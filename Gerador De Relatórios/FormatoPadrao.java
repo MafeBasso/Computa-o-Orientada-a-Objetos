8a0550012390b16331737eb4c86dbf0cc9f4879f
MafeBasso <49072823+MafeBasso@users.noreply.github.com>
2020-07-27 17:25:41 -0300

    Create FormatoPadrao.java

---
+++ Gerador De Relatórios/FormatoPadrao.java
@@ -0,0 +1,11 @@
+public class FormatoPadrao implements Formato {
+	public String padrao = "";
+	
+	public String formatacao() {
+		return padrao;
+	}
+	
+	public String finalizaFormatacao() {
+		return padrao;
+	}
+}
